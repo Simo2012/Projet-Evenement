35b3a729d6f541191e4deccbaa997e85a67dfcea
GHAZI Mohammed <s.ghazi06@gmail.com>
2015-08-17 23:49:26 +0200

    Nouveau Proje

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Event\EventBundle\EventEventBundle(),
             new Event\UserBundle\EventUserBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            //new FOS\FacebookBundle\FOSFacebookBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
